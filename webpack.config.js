3020a3b40b1985dfc038ce3abb822eef6716ee30
Brad Trick <btrick@gmail.com>
2021-03-08 03:17:32 -0500

    Initial commit; gems installed

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './frontend/slacker.jsx',
+  output: { 
+    path: 'app/assets/javascript',
+    filename: './bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
