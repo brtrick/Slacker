ede7f32b4496b3c91a80b4b30c6a57497b47df21
Brad Trick <btrick@gmail.com>
2021-03-15 18:23:36 -0400

    Added files to display basic webpage

---
+++ frontend/components/app.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route } from "react-router-dom"
+// import LoginFormContainer from "./login_form_container";
+// import SignupFormContainer from "./signup_form_container";
+const App = () => (
+    <div>
+        <header>
+            <h1>Welcome to Slacker</h1>
+            {/* <Route exact path="/" component={GreetingContainer} />
+            <Route path="/login" component={LoginFormContainer} />
+            <Route path="/signup" component={SignupFormContainer} /> */}
+        </header>
+    </div>
+);
+
+export default App;
---
+++ frontend/store/store.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "../reducers/root_reducer";
+import thunk from "redux-thunk";
+
+const configureStore = (preLoadedState = {}) => {
+    return createStore(rootReducer, preLoadedState, applyMiddleware(thunk));
+}
+
+export default configureStore;
