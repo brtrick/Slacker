c1ad459734b2a3be582cf6dd9846cb9ccc093a8d
Brad Trick <btrick@gmail.com>
2021-03-16 03:44:09 -0400

    Add session reducer, actions, and util

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux";
-// import sessionReducer from "./session_reducer";
+import sessionReducer from "./session_reducer";
 // import entitiesReducer from "./entities_reducer";
 // import errorsReducer from "./errors_reducer";
 
 export default combineReducers({
-    
+    session: sessionReducer,
 })
