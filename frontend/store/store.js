db4329b0fd733c9f962e3578931ce01524622d3b
Brad Trick <btrick@gmail.com>
2021-03-19 09:27:29 -0400

    Comment out logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/root_reducer";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
+//import logger from "redux-logger";
 
 const defaultState = {
     entities: {
@@ -10,7 +10,8 @@ const defaultState = {
     session:{}
 }
 const configureStore = (preLoadedState = defaultState) => {
-    return createStore(rootReducer, preLoadedState, applyMiddleware(thunk, logger));
+    return createStore(rootReducer, preLoadedState, applyMiddleware(thunk));
+    // return createStore(rootReducer, preLoadedState, applyMiddleware(thunk, logger));
 }
 
 export default configureStore;
